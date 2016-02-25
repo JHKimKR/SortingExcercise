94386a0c2f7f4ca1eb280d51efc04084b7d187c5
KimJongHan <kjh929y@gmail.com>
2016-02-25 21:23:28 +0900

    Commit First BubbleSort

--- BubbleSort/main.c
+++ BubbleSort/main.c
@@ -7,9 +7,82 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main() {
+    
+    int num = rand() % 100 + 1;
+    int pbArray [num];
+    int temp;
+    
+    printf("%d\n", num);
+    
+    for(int index = 0; index < num; index++)
+    {
+        pbArray[index] = rand() % 100 + 1;
+        
+        printf("%d\t", pbArray[index]);
+        
+    }
+    
+    printf("\n");
+    
+    /*
+    for(int index2 = 0; index2 < num; index2++)
+    {
+        for(int index = num-1; index > 0; index--)
+        {
+     
+            if(pbArray[index] < pbArray[index-1])
+            {
+                temp = pbArray[index];
+
+                pbArray[index] = pbArray[index -1];
+
+                pbArray[index-1] = temp;
+
+                
+            }
+            else
+                continue;
+        }
+
+    }
+    */
+    
+    
+    
+    for(int index2 = num-1; index2 > 0; index2--)
+    {
+        for(int index = 0; index < num-1; index++)
+        {
+            
+            if(pbArray[index+1] < pbArray[index])
+            {
+                temp = pbArray[index+1];
+                
+                pbArray[index+1] = pbArray[index];
+                
+                pbArray[index] = temp;
+                
+                
+            }
+            else
+                continue;
+        }
+        
+    }
+    
+    for(int index = 0; index < num; index++)
+    {
+     
+        printf("%d\t", pbArray[index]);
+        
+    }
+    
+    
+    
     return 0;
 }
